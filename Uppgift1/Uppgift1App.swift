702184678d78df5320849279008317280159d98e
Alexander Lins <alexanderlins88@icloud.com>
2023-10-02 08:39:12 +0200

    Initial Commit

---
+++ Uppgift1/Uppgift1App.swift
@@ -0,0 +1,17 @@
+//
+//  Uppgift1App.swift
+//  Uppgift1
+//
+//  Created by Alexander Lins on 2023-10-02.
+//
+
+import SwiftUI
+
+@main
+struct Uppgift1App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
